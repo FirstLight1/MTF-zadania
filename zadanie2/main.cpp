ab44a43c055dd3feb2a80421f44640b6beb2ddfd
Lukas Kamen <kamen.lukas1990@gmail.com>
2025-04-06 18:42:55 +0200

    almost working, maybe leaking leaking memory ¯\_(ツ)_/¯

--- zadanie2/main.cpp
+++ zadanie2/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
-//#include "shellSort.h"
+#include "shellSort.h"
 #include "mergeSort.h"
 
 #define nLogN 10000000
@@ -9,37 +9,81 @@
 
 using namespace std;
 
+typedef void(*sortFunc)(unsigned int*, int, int);
+
 void randomNumbers(unsigned int *arr);
+void vytvorArr(unsigned int *arr1, unsigned int *arr2, int dlzka);
+bool zoradene(unsigned int *arr, int dlzka);
+double zmerajCas(sortFunc sort, unsigned int* arr, int x, int dlzka);
 
 int main(){
     srand(time(NULL));
-    //unsigned int *arr = new unsigned int[20];
+    unsigned int *arr1 = new unsigned int[n2];
+    unsigned int *arr2 = new unsigned int[n2];
     
-    int dlzka = 10;
-    unsigned int arr[] = {3, 5, 7, 1, 6, 2, 9, 8, 4, 0};
-    //randomNumbers(arr);
-    //int stred = 10 / 2;
-    //shellSort(arr, 10, stred);
-    mergeSort(arr, 0, dlzka);
+    double shellT1, shellT2, shellT3, mergeT1, mergeT2, mergeT3;
     
-    
-    for (int i = 0; i < 10; i++)
-    {
-        cout<<arr[i]<<endl;
-    }
-    
+    int stred = n2 / 2;
 
-    //delete arr;
+    vytvorArr(arr1, arr2, n2);
+    shellT1 = zmerajCas(*shellSort, arr1, stred, n2);
+    mergeT1 = zmerajCas(*mergeSort, arr2, 0 ,n2);
+    
+    vytvorArr(arr1, arr2, n2);
+    shellT2 = zmerajCas(*shellSort, arr1, stred, n2);
+    mergeT2 = zmerajCas(*mergeSort, arr2, 0 ,n2);
+    
+    vytvorArr(arr1, arr2, n2);
+    shellT3 = zmerajCas(*shellSort, arr1, stred, n2);
+    mergeT3 = zmerajCas(*mergeSort, arr2, 0 ,n2);
+
+    double priemer;
+    priemer = (shellT1 + shellT2 + shellT3) / 3;
+    cout<<"Priemerny čas shell sortu:"<<priemer<<endl;
+
+    priemer = (mergeT1 + mergeT2 + mergeT3) / 3;
+    cout<<"Priemerny čas merge sortu:"<<priemer<<endl;
+    
+    delete[] arr1;
+    delete[] arr2;
     return 0;
-    
 }
-
 
 void randomNumbers(unsigned int *arr){
-    for (int i = 0; i < 9; i++){
+    for (int i = 0; i < n2; i++){
     arr[i] = rand();
+    }
 }
 
+void vytvorArr(unsigned int *arr1, unsigned int *arr2, int dlzka){
+    randomNumbers(arr1);
+    for (int i = 0; i < n2; i++)
+    {
+        arr2[i] = arr1[i];
+    }
+}
+
+bool zoradene(unsigned int *arr, int dlzka){
+    for (int i = 0; i < dlzka; i++){
+        if (arr[i] > arr[i+1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+double zmerajCas(sortFunc sort, unsigned int* arr, int x, int dlzka){
+    clock_t start = clock();
+    double t;
+
+    sort(arr, x, dlzka);
+    if(zoradene(arr, n2)){
+        start = clock() - start;
+    }
+    t = ((double) start) / CLOCKS_PER_SEC;
+
+    return t;
 }
 
 
