576768f39d413e18e21200174fe954ee42f4dabc
FirstLight1 <159583498+FirstLight1@users.noreply.github.com>
2025-04-08 18:46:14 +0200

    Delete zadanie2 directory

--- zadanie2/mergeSort.h
+++
@@ -1,9 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void mergeSort(unsigned int *array,int zaciatok, int koniec);
-
-#ifdef __cplusplus
-}
-#endif
--- zadanie2/shellSort.h
+++
@@ -1,9 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void shellSort(unsigned int * array, int dlzka, int gap);
-
-#ifdef __cplusplus
-}
-#endif
